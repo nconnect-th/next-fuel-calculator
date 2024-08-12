ae079cfb2d70e8c71fdb34dafd2a17a703b30e92
nConnect <TapanaponS@Gmail.com>
2024-08-12 15:45:08 +0700

    Update 1545

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        {/* Additional content here */}
+      </div>
+    );
+  }
+  
