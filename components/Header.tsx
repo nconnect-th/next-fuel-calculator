7618172ffd642a74872fb8ff96b19edac26f0854
nConnect <TapanaponS@Gmail.com>
2024-08-12 17:14:22 +0700

    Update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+// components/Header.tsx
+"use client";
+
 import { useState } from 'react';
 
 const navigation = [
@@ -19,7 +22,6 @@ export default function Header() {
         <div className="flex h-16 justify-between">
           <div className="flex">
             <div className="-ml-2 mr-2 flex items-center md:hidden">
-              {/* Mobile menu button */}
               <button
                 type="button"
                 className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
